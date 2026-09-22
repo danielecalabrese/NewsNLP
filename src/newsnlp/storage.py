1287a61011534adce5de2f946a6197c45141e2a4
Daniele <daniele.calabrese@hotmail.it>
2026-09-22 10:18:24 +0200

    feat: add article storage interface

---
+++ src/newsnlp/storage.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from newsnlp.models.processed_article import ProcessedArticle
+
+
+class ArticleStorage(ABC):
+    """Interface for persistent storage of processed articles."""
+
+    @abstractmethod
+    def save(self, article: ProcessedArticle) -> None:
+        """Persist a processed article."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, article_id: str) -> ProcessedArticle | None:
+        """Retrieve a processed article by its ID."""
+        raise NotImplementedError
